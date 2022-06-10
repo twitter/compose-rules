6cc6b0ec988b5fd5bb54b633870cebca7f511d28
Nacho Lopez <nacho@nlopez.io>
2022-06-10 21:08:16 +0200

    Add rule to detect non-remembered mutable/derivedStateOf
    When used in a Composable, `mutableStateOf` and `derivedStateOf` should be remembered. It is easy to forget about this, and the consecuences are annoying to deal with. So here we are.

---
+++ rules/ktlint/src/main/kotlin/com/twitter/rules/ktlint/compose/ComposeRememberMissingCheck.kt
@@ -0,0 +1,58 @@
+package com.twitter.rules.ktlint.compose
+
+import com.twitter.rules.core.findChildrenByClass
+import com.twitter.rules.core.isComposable
+import com.twitter.rules.core.ktlint.Emitter
+import com.twitter.rules.core.ktlint.TwitterKtRule
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
+
+class ComposeRememberMissingCheck : TwitterKtRule("compose-remember-missing-check") {
+
+    override fun visitFunction(function: KtFunction, autoCorrect: Boolean, emitter: Emitter) {
+        if (!function.isComposable) return
+
+        // To keep memory consumption in check, we first traverse down until we see one of our known functions
+        // that need remembering
+        function.findChildrenByClass<KtCallExpression>()
+            .filter { MethodsThatNeedRemembering.contains(it.calleeExpression?.text) }
+            // Only for those, we traverse up to [function], to see if it was actually remembered
+            .filterNot { it.isRemembered(function) }
+            // If it wasn't, we show the error
+            .forEach { callExpression ->
+                when (callExpression.calleeExpression!!.text) {
+                    "mutableStateOf" -> emitter.report(callExpression.startOffset, MutableStateOfNotRemembered, false)
+                    "derivedStateOf" -> emitter.report(callExpression.startOffset, DerivedStateOfNotRemembered, false)
+                }
+            }
+    }
+
+    private fun KtCallExpression.isRemembered(stopAt: PsiElement): Boolean {
+        var current: PsiElement = parent
+        while (current != stopAt) {
+            (current as? KtCallExpression)?.let { callExpression ->
+                if (callExpression.calleeExpression?.text?.startsWith("remember") == true) return true
+            }
+            current = current.parent
+        }
+        return false
+    }
+
+    companion object {
+        private val MethodsThatNeedRemembering = setOf(
+            "derivedStateOf",
+            "mutableStateOf"
+        )
+        val DerivedStateOfNotRemembered = errorMessage("derivedStateOf")
+        val MutableStateOfNotRemembered = errorMessage("mutableStateOf")
+
+        private fun errorMessage(name: String): String = """
+            Using `$name` in a @Composable function without it being inside of a remember function.
+            If you don't remember the state instance, a new state instance will be created when the function is recomposed.
+
+            See LINK TBD s for more information.
+        """.trimIndent()
+    }
+}
