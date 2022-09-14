b06a667a6bf1a5c6351f4bb2754e1715839b2b61
Manuel Martos <mmartos@degirona.info>
2022-09-14 11:06:13 +0200

    Tweak PreviewPublic logic to avoid flagging valid code (#57)
    * Tweak PreviewPublic logic to avoid flagging valid code
    
    * Add test also to ktlint package
    
    * Fixes #53
    
    * Fix spotless check

--- rules/common/src/main/kotlin/com/twitter/compose/rules/ComposePreviewPublic.kt
+++ rules/common/src/main/kotlin/com/twitter/compose/rules/ComposePreviewPublic.kt
@@ -8,14 +8,17 @@ import com.twitter.rules.core.util.isPreview
 import com.twitter.rules.core.util.isPreviewParameter
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
 
 class ComposePreviewPublic : ComposeKtVisitor {
 
     override fun visitComposable(function: KtFunction, autoCorrect: Boolean, emitter: Emitter) {
         // We only want previews
         if (!function.isPreview) return
-        // ...that have provided params to them (so we know they are only for previews)
-        if (function.valueParameters.none { it.isPreviewParameter }) return
+        // ...that if it is public, none of it's params is tagged as preview
+        if (function.isPublic && function.valueParameters.none { it.isPreviewParameter }) return
+        // ...and if it isn't public, all params are tagged as preview
+        if (!function.isPublic && function.valueParameters.all { it.isPreviewParameter }) return
 
         emitter.report(function, ComposablesPreviewShouldNotBePublic, true)
         if (autoCorrect) {
